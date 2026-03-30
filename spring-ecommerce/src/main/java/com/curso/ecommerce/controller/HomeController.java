e5b5dd96a5b27f6ad019f58e8e20e7fb5c4fd9c2
Ketzyah <juan20022002franco@gmail.com>
2026-03-30 17:37:25 -0500

    mostrar parametros en ver producto

--- spring-ecommerce/src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ spring-ecommerce/src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.curso.ecommerce.controller;
 
+import com.curso.ecommerce.model.Producto;
 import com.curso.ecommerce.service.ProductoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
@@ -27,7 +30,11 @@ public class HomeController {
     }
 
     @GetMapping("/productohome/{id}")
-    public String productHome(@PathVariable Integer id){
+    public String productHome(@PathVariable Integer id, Model model){
+        Producto producto = new Producto();
+        Optional<Producto> productoOptional = productoService.get(id);
+        producto = productoOptional.get();
+        model.addAttribute("producto", producto);
 
         log.info("id producto enviado como parameto {}", id);
         return "usuario/productohome";
