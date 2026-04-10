4ae8d467f4455dd9d04540427cc55cc215426538
Ketzyah <juan20022002franco@gmail.com>
2026-04-10 15:52:23 -0500

    validacion para no añadir doblemente producto al carrito, y funcion para dirigir al carrito desde cabecera usuario

--- spring-ecommerce/src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ spring-ecommerce/src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -64,7 +64,15 @@ public class HomeController {
         detalleOrden.setTotal(producto.getPrice() * cantidad);
         detalleOrden.setProducto(producto);
 
-        detalles.add(detalleOrden);
+        //validar que producto no se repita
+        Integer idProducto = producto.getId();
+        boolean ingresado = detalles.stream().anyMatch(p -> p.getProducto().getId() == idProducto);
+
+        if(!ingresado){
+            detalles.add(detalleOrden);
+        }
+
+
 
         sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
 
@@ -72,6 +80,15 @@ public class HomeController {
         model.addAttribute("cart", detalles);
         model.addAttribute("orden", orden);
 
+        return "usuario/carrito";
+    }
+
+
+    @GetMapping("/getCart")
+    public String getCart(Model model){
+
+        model.addAttribute("cart", detalles);
+        model.addAttribute("orden", orden);
         return "usuario/carrito";
     }
 
