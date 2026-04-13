ada4b378c9ceb0985ada494d72a9de5176f7508c
Ketzyah <juan20022002franco@gmail.com>
2026-04-13 17:20:35 -0500

    añadida funcionalidad para verificacion de correo en login y solucionado redireccionar Ver producto en vista home/administrador

--- spring-ecommerce/src/main/java/com/curso/ecommerce/controller/UsuarioController.java
+++ spring-ecommerce/src/main/java/com/curso/ecommerce/controller/UsuarioController.java
@@ -2,6 +2,7 @@ package com.curso.ecommerce.controller;
 
 import com.curso.ecommerce.model.Usuario;
 import com.curso.ecommerce.service.IUsuarioService;
+import jakarta.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/usuario")
@@ -35,6 +39,31 @@ public class UsuarioController {
         usuario.setTipo("USER");
         usuarioService.save(usuario);
 
+        return "redirect:/";
+    }
+
+
+    @GetMapping("/login")
+    public String login(){
+
+        return "usuario/login";
+    }
+
+    @PostMapping("/acceder")
+    public String acceder(Usuario usuario, HttpSession session){
+        Optional<Usuario> user = usuarioService.findByEmail(usuario.getEmail());
+        logger.info("usuario de db: {}", user.get());
+
+        if(user.isPresent()){
+            session.setAttribute("idUsuario", user.get().getId());
+            if(user.get().getTipo().equals("ADMIN")){
+                return "redirect:/administrador";
+            }else {
+                return "redirect:/";
+            }
+        }else{
+            logger.info("usuario no existe");
+        }
         return "redirect:/";
     }
 
