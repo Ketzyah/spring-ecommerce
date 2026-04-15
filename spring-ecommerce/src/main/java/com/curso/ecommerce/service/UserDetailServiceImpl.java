fd2094741ce46d26e852a67a1139435aaa1f5b70
Ketzyah <juan20022002franco@gmail.com>
2026-04-15 16:09:12 -0500

    video 48

---
+++ spring-ecommerce/src/main/java/com/curso/ecommerce/service/UserDetailServiceImpl.java
@@ -0,0 +1,48 @@
+package com.curso.ecommerce.service;
+
+
+import com.curso.ecommerce.model.Usuario;
+import jakarta.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+
+
+    @Autowired
+    private IUsuarioService usuarioService;
+    @Autowired
+    private BCryptPasswordEncoder bCrypt;
+    @Autowired
+    HttpSession session;
+
+    private Logger log = LoggerFactory.getLogger(UserDetailServiceImpl.class);
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        log.info("Esto es el username ");
+        Optional<Usuario> optionalUser= usuarioService.findByEmail(username);
+        if(optionalUser.isPresent()){
+            log.info("esto es el id del usuario {}", optionalUser.get().getId());
+            session.setAttribute("idUsuario", optionalUser.get().getId());
+            Usuario usuario = optionalUser.get();
+            return User.builder().username(usuario.getName()).password(bCrypt.encode(usuario.getPassword())).roles(usuario.getTipo()).build();
+        }else{
+            throw new UsernameNotFoundException("Usuario no encontrado");
+        }
+
+    }
+}
